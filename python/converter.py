9e529377adc9e8981f9d867e6c000238c0214667
Joseph K. Bradley <joseph.kurata.bradley@gmail.com>
2015-10-15 09:37:58 -0700

    Added support for conversions from Spark to SKL

--- python/converter.py
+++ python/converter.py
@@ -19,11 +19,15 @@ ClassNames = namedtuple('ClassNames', ['jvm', 'py'])
 
 class Converter(object):
     """
-    TODO
+    Class for converting between scikit-learn and Spark ML models
     """
 
     def __init__(self, sc):
+        """
+        :param sc: SparkContext
+        """
         self.sc = sc
+        # For conversions sklearn -> Spark
         self._skl2spark_classes = {
             SKL_LogisticRegression :
                 ClassNames("org.apache.spark.ml.classification.LogisticRegressionModel",
@@ -33,8 +37,21 @@ class Converter(object):
                            LinearRegressionModel)
         }
         self._supported_skl_types = self._skl2spark_classes.keys()
+        # For conversions Spark -> sklearn
+        self._spark2skl_classes =\
+            dict([(self._skl2spark_classes[skl].py, skl) for skl in self._skl2spark_classes.keys()])
+        self._supported_spark_types = [x.py for x in self._skl2spark_classes.values()]
 
     def toSpark(self, model):
+        """
+        Convert a scikit-learn model to a Spark ML model from the Pipelines API (spark.ml).
+        Currently supported models:
+          sklearn.linear_model.LogisticRegression (binary classification only, not multiclass)
+          sklearn.linear_model.LinearRegression
+        :param model: scikit-learn model
+        :return: Spark ML model with equivalent predictive behavior.
+                 Currently, parameters or arguments for training are not copied.
+        """
         if isinstance(model, SKL_LogisticRegression) or isinstance(model, SKL_LinearRegression):
             return self._toSparkGLM(model)
         else:
@@ -44,6 +61,7 @@ class Converter(object):
 
     def _toSparkGLM(self, model):
         """ Private method for converting a GLM to a Spark model
+        TODO: Add model parameters as well.
         """
         skl_cls = type(model)
         py_cls = self._skl2spark_classes[skl_cls].py
@@ -65,3 +83,33 @@ class Converter(object):
         else:
             raise Exception("Converter.toSpark experienced unknown error when trying to convert" +
                             " a model of type: " + type(model) + "  " + len(numpy.shape(weights)))
+
+    def toSKLearn(self, model):
+        """
+        Convert a Spark MLlib model from the Pipelines API (spark.ml) to a scikit-learn model.
+        Currently supported models:
+          pyspark.ml.classification.LogisticRegressionModel
+          pyspark.ml.regression.LinearRegressionModel
+        :param model: Spark ML model
+        :return: scikit-learn model with equivalent predictive behavior.
+                 Currently, parameters or arguments for training are not copied.
+        """
+        if isinstance(model, LogisticRegressionModel) or isinstance(model, LinearRegressionModel):
+            return self._toSKLGLM(model)
+        else:
+            supported_types = map(lambda t: type(t), self._supported_spark_types)
+            raise ValueError("Converter.toSKLearn cannot convert type: %s.  Supported types: %s" %
+                             (type(model), ", ".join(supported_types)))
+
+    def _toSKLGLM(self, model):
+        """ Private method for converting a GLM to a scikit-learn model
+        TODO: Add model parameters as well.
+        """
+        py_cls = type(model)
+        skl_cls = self._spark2skl_classes[py_cls]
+        intercept = model.intercept
+        weights = model.weights
+        skl = skl_cls()
+        skl.intercept_ = numpy.float64(intercept)
+        skl.coef_ = weights.toArray()
+        return skl
--- python/tests.py
+++ python/tests.py
@@ -16,8 +16,8 @@ from sklearn.linear_model import LogisticRegression as SKL_LogisticRegression
 from sklearn.linear_model import LinearRegression as SKL_LinearRegression
 
 from pyspark import SparkContext
-from pyspark.ml.classification import LogisticRegressionModel
-from pyspark.ml.regression import LinearRegressionModel
+from pyspark.ml.classification import LogisticRegression, LogisticRegressionModel
+from pyspark.ml.regression import LinearRegression, LinearRegressionModel
 from pyspark.mllib.linalg import Vectors
 from pyspark.sql import SQLContext
 
@@ -34,6 +34,8 @@ class MLlibTestCase(unittest.TestCase):
                               [-1,-2,3], [1,-2,-3], [-1,2,-3],
                               [-1,-2,-3]])
         self.y = numpy.array([1, 0, 1, 1, 0, 1, 0, 0])
+        data = [(float(self.y[i]), Vectors.dense(self.X[i])) for i in range(len(self.y))]
+        self.df = self.sql.createDataFrame(data, ["label", "features"])
 
 class ConverterTests(MLlibTestCase):
 
@@ -41,29 +43,40 @@ class ConverterTests(MLlibTestCase):
         super(ConverterTests, self).setUp()
         self.converter = Converter(self.sc)
 
-    def test_LogisticRegression(self):
+    def _compare_GLMs(self, skl, spark):
+        """ Compare weights, intercept of sklearn, Spark GLMs
+        """
+        skl_weights = Vectors.dense(skl.coef_.flatten())
+        self.assertEqual(skl_weights, spark.weights)
+        self.assertEqual(skl.intercept_, spark.intercept)
+
+    def test_LogisticRegression_skl2spark(self):
         skl_lr = SKL_LogisticRegression().fit(self.X, self.y)
-        skl_weights = Vectors.dense(skl_lr.coef_.flatten())
-        skl_intercept = skl_lr.intercept_
         lr = self.converter.toSpark(skl_lr)
         self.assertTrue(isinstance(lr, LogisticRegressionModel),
                         "Expected LogisticRegressionModel but found type %s" % type(lr))
-        spark_weights = lr.weights
-        spark_intercept = lr.intercept
-        self.assertEqual(skl_weights, spark_weights)
-        self.assertEqual(skl_intercept, spark_intercept)
+        self._compare_GLMs(skl_lr, lr)
 
-    def test_LinearRegression(self):
+    def test_LinearRegression_skl2spark(self):
         skl_lr = SKL_LinearRegression().fit(self.X, self.y)
-        skl_weights = Vectors.dense(skl_lr.coef_.flatten())
-        skl_intercept = skl_lr.intercept_
         lr = self.converter.toSpark(skl_lr)
         self.assertTrue(isinstance(lr, LinearRegressionModel),
                         "Expected LinearRegressionModel but found type %s" % type(lr))
-        spark_weights = lr.weights
-        spark_intercept = lr.intercept
-        self.assertEqual(skl_weights, spark_weights)
-        self.assertEqual(skl_intercept, spark_intercept)
+        self._compare_GLMs(skl_lr, lr)
+
+    def test_LogisticRegression_spark2skl(self):
+        lr = LogisticRegression().fit(self.df)
+        skl_lr = self.converter.toSKLearn(lr)
+        self.assertTrue(isinstance(skl_lr, SKL_LogisticRegression),
+                        "Expected sklearn LogisticRegression but found type %s" % type(skl_lr))
+        self._compare_GLMs(skl_lr, lr)
+
+    def test_LinearRegression_spark2skl(self):
+        lr = LinearRegression().fit(self.df)
+        skl_lr = self.converter.toSKLearn(lr)
+        self.assertTrue(isinstance(skl_lr, SKL_LinearRegression),
+                        "Expected sklearn LinearRegression but found type %s" % type(skl_lr))
+        self._compare_GLMs(skl_lr, lr)
 
 
 if __name__ == "__main__":
