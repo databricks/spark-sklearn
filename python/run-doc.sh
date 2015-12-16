ccb911625a7343e6cff8b8d69b0e1884085c4c9f
Timothy Hunter <timhunter@databricks.com>
2015-12-16 14:08:48 -0800

    documentation

---
+++ python/run-doc.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# assumes run from python/ directory
+if [ -z "$SPARK_HOME" ]; then
+    echo 'You need to set $SPARK_HOME to run these tests.' >&2
+    exit 1
+fi
+
+LIBS=""
+for lib in "$SPARK_HOME/python/lib"/*zip ; do
+  LIBS=$LIBS:$lib
+done
+
+export PYTHONPATH=$PYTHONPATH:$SPARK_HOME/python$LIBS:.
+
+sphinx-build doc/ doc_gen
