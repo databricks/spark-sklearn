89c0f547ee978ff1cfd58cabedc064ce0e927dea
Timothy Hunter <timhunter@databricks.com>
2016-03-16 11:40:01 -0700

    cleanup of testing scripts

--- python/run-tests.sh
+++ python/run-tests.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 # assumes run from python/ directory
 if [ -z "$SPARK_HOME" ]; then
     echo 'You need to set $SPARK_HOME to run these tests.' >&2
@@ -11,6 +12,21 @@ for lib in "$SPARK_HOME/python/lib"/*zip ; do
   LIBS=$LIBS:$lib
 done
 
-export PYTHONPATH=$PYTHONPATH:$SPARK_HOME/python$LIBS:.
+# The current directory of the script.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-nosetests
+#export PYSPARK_SUBMIT_ARGS="--jars pyspark-shell "
+
+export PYTHONPATH=$PYTHONPATH:$SPARK_HOME/python:$LIBS:.
+
+export PYTHONPATH=$PYTHONPATH:spark_sklearn
+
+# Return on any failure
+set -e
+
+# Run test suites
+
+# Horrible hack for spark 1.4: we manually remove some log lines to stay below the 4MB log limit on Travis.
+# To remove when we ditch spark 1.4.
+nosetests -v --all-modules -w $DIR  2>&1 | grep -vE "INFO (PythonRunner|ContextCleaner|ShuffleBlockFetcherIterator|MapOutputTrackerMaster|TaskSetManager|Executor|MemoryStore|CacheManager|BlockManager|DAGScheduler|PythonRDD|TaskSchedulerImpl|ZippedPartitionsRDD2)"
+
