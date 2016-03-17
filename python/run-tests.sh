777ed9e8d37aeb98e6c265ae51017e6c6854b4f4
Timothy Hunter <timhunter@databricks.com>
2016-03-17 09:56:05 -0700

    remove debug output

--- python/run-tests.sh
+++ python/run-tests.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 # assumes run from python/ directory
 if [ -z "$SPARK_HOME" ]; then
     echo 'You need to set $SPARK_HOME to run these tests.' >&2
@@ -23,10 +22,6 @@ export PYTHONPATH=$PYTHONPATH:spark_sklearn
 
 # Use the miniconda environment:
 export PYTHONPATH=$PYTHONPATH:/home/travis/miniconda/envs/test-environment/lib/python2.7/site-packages/
-
-echo "TEST"
-python -c "import scipy.sparse; print scipy.sparse"
-echo "TEST DONE"
 
 # Return on any failure
 set -e
