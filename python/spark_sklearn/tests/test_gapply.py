879f22288ecef83c25e480cba40b0d519d5dc2de
Vladimir Feinberg <vf@databricks.com>
2016-06-23 16:54:35 -0700

    Skipping test while waiting for spark fix

--- python/spark_sklearn/tests/test_gapply.py
+++ python/spark_sklearn/tests/test_gapply.py
@@ -41,6 +41,7 @@ class GapplyTests(unittest.TestCase):
         gd = emptyLongLongDF.groupBy("a")
         self.assertEqual(gapply(gd, _emptyFunc, longLongSchema, "b").collect(), [])
 
+    @unittest.skip("Generating an empty dataframe after exploding fails, see SPARK-16179")
     def test_gapply_empty_schema(self):
         longLongSchema = StructType().add("a", LongType()).add("b", LongType())
         emptyLongLongDF = self.spark.createDataFrame([(1, 2)], schema=longLongSchema)
