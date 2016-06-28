9b248f13858bf153262767b54c9627a22ed39780
Vladimir Feinberg <vf@databricks.com>
2016-06-28 10:08:20 -0700

    Added module documentation for spark_sklearn.keyed_models

--- python/gen-doc.sh
+++ python/gen-doc.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
-# assumes run from python/ directory
 if [ -z "$SPARK_HOME" ]; then
     echo 'You need to set $SPARK_HOME to run these tests.' >&2
     exit 1
 fi
+
+# The current directory of the script.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 LIBS=""
 for lib in "$SPARK_HOME/python/lib"/*zip ; do
   LIBS=$LIBS:$lib
 done
 
-export PYTHONPATH=$PYTHONPATH:$SPARK_HOME/python$LIBS:.
+export PYTHONPATH=$PYTHONPATH:$SPARK_HOME/python$LIBS:$DIR
 
-sphinx-build -E -a -j8 doc/ doc_gen/
+sphinx-build -E -a -j4 $DIR/doc/ $DIR/doc_gen/
