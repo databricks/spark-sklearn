c42d85824283f6a9b3f597bd8feabe37cf020b5d
Sid Murching <sid.murching@databricks.com>
2017-09-14 11:28:30 -0700

    Use scikit-learn >= 0.18.1 consistently. The scikit-learn tests will break if we try to run them using spark-sklearn -- TOOD fix this eventually

--- python/setup.py
+++ python/setup.py
@@ -19,7 +19,7 @@ CLASSIFIERS = [
     "Programming Language :: Python",
     "Topic :: Scientific/Engineering",
 ]
-INSTALL_REQUIRES = ["scikit-learn == 0.18.1"]
+INSTALL_REQUIRES = ["scikit-learn >= 0.18.1"]
 
 # Project root
 ROOT = os.path.abspath(os.getcwd() + "/")
