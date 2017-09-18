41cf622c193330d42baa36449f11c689976a7663
Sid Murching <sid.murching@databricks.com>
2017-09-18 14:37:43 -0700

    Add upper version bound on scikit-learn, tighten GridSearchCV doctest

--- python/spark_sklearn/grid_search.py
+++ python/spark_sklearn/grid_search.py
@@ -127,7 +127,7 @@ class GridSearchCV(BaseSearchCV):
                          kernel='rbf', max_iter=-1, probability=False,
                          random_state=None, shrinking=True, tol=...,
                          verbose=False),
-           fit_params=..., iid=..., n_jobs=1,
+           fit_params={}, iid=..., n_jobs=1,
            param_grid=..., pre_dispatch=..., refit=...,
            scoring=..., verbose=...)
     >>> sorted(clf.cv_results_.keys())
